142c29c7f580c04c0f46efd5119f8fac5512ab38
Saman Sandhu <saman_sandhu@outlook.com>
2020-02-29 13:33:56 -0500

    Added file containing global constants

---
+++ app/src/main/java/com/example/ravenous/Global.kt
@@ -0,0 +1,5 @@
+package com.example.ravenous
+
+const val LOG_TAG = "Sam"
+const val WEB_SERVICE_URL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/"
+const val API_KEY = "FkiKuFnSiRCinH0EHKTgq7a1jPl6bBIS7QXyWPUsUHJL_qHVAC6y5k6CPlbsvEMalJE7pgrnSMgtGdg6devyMiuVdUCvnKbBpfnuhs5ZDoQdFUso8Tx3lWqWLy8xXHYx"
