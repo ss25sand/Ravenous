b159938de81b70c0dc13308f54b82bd3341e20f8
Saman Sandhu <saman_sandhu@outlook.com>
2020-03-29 22:09:25 -0400

    Added list/grid option menu and helper for preference persistence

---
+++ app/src/main/java/com/example/ravenous/utils/PrefsHelper.kt
@@ -0,0 +1,20 @@
+package com.example.ravenous.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+const val ITEM_TYPE_KEY = "item_type_key"
+
+class PrefsHelper {
+    companion object {
+        private fun preferences(context: Context): SharedPreferences =
+            context.getSharedPreferences("default", 0) // name of xml file, available everywhere
+
+        fun setItemType(context: Context, type: String) {
+            preferences(context).edit().putString(ITEM_TYPE_KEY, type).apply()
+        }
+
+        fun getItemType(context: Context): String =
+            preferences(context).getString(ITEM_TYPE_KEY, "list")!!
+    }
+}
