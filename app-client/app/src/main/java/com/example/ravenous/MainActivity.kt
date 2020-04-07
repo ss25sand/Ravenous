0ca9d08f0fd23f6e3dd7a1f8b4c78312319d5962
Saman Sandhu <saman_sandhu@outlook.com>
2020-04-06 22:18:47 -0400

    Build and location changes:     - added multidex and google play location service libraries     - added view binding support     - upgraded kotlin version     - added code to get last known location of user and save it in the view model

--- app-client/app/src/main/java/com/example/ravenous/MainActivity.kt
+++ app-client/app/src/main/java/com/example/ravenous/MainActivity.kt
@@ -1,12 +1,70 @@
 package com.example.ravenous
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.ravenous.ui.shared.SharedViewModel
+import com.google.android.gms.location.LocationServices
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val permissionAccessCoarseLocationApproved = ActivityCompat
+            .checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
+
+        if (permissionAccessCoarseLocationApproved) {
+            // Permission has already been granted
+            startRecommendationRequest()
+        } else {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                MY_PERMISSIONS_REQUEST_COARSE_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_COARSE_LOCATION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay!
+                    startRecommendationRequest()
+                }
+                return
+            }
+        }
+    }
+
+    fun startRecommendationRequest() {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            Log.i(LOG_TAG, (location ?: "No last location value").toString())
+            location?.let {
+                val geocoder = Geocoder(this)
+                val addresses: List<Address> = geocoder.getFromLocation(it.latitude, it.latitude, 1)
+
+                val viewModel = ViewModelProvider(this).get(SharedViewModel::class.java)
+                viewModel.lastLocation.value = addresses.first()
+            }
+
+        }
     }
 }
