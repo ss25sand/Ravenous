04ff01cdd37450ea8097260cd8159996225c64f7
Saman Sandhu <saman_sandhu@outlook.com>
2020-04-10 19:44:43 -0400

    Added mock test data for recommendation service

--- backend/apps/app.py
+++ backend/apps/app.py
@@ -16,6 +16,8 @@ def top_ratings(user_id, count):
     city = request.args['city']
     state = request.args['state']
     top_rating_list = recommendation_engine.get_top_ratings(user_id, count, city, state)
+    # mock test data
+    # top_rating_list = ["FK40a1dGkHYApwFKpmMlfg", "gbXd_463dTvZsS7ilBlK7A", "nWSPZ4ZoOFI0Vlzl-f7duw", "U4Y-uVZGkvTrtIWXxGia-A", "JKIxjnraqPqBMNTtlEBaaQ", "97d7Qy70tyFoJJaGVuCaqg", "XsSXlTHBjQAoXGou86bENA", "fx4K0PoKGVyZt7GgL_6agg", "7AeSmKi0aAhxNHo0e03O7g", "GxL2y7Fgrq4ph8G7EoVsAw"]
     return json.dumps(top_rating_list)
  
 @main.route('/<string:user_id>/rating', methods = ['POST'])
